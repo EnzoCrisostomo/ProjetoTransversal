dfaf9d73c6962294b0d0f229158099e03f3e0874
Enzo Crisóstomo <enzo.crisostomo2000@gmail.com>
2022-05-25 13:40:08 -0300

    gpu notebook

--- ProjetoTransversal/src/main.cpp
+++ ProjetoTransversal/src/main.cpp
@@ -3,6 +3,18 @@
 #include "Options/Options.h"
 #include "Application.h"
 
+//enable optimus NVIDIA gpu
+extern "C"
+{
+	__declspec(dllexport) unsigned long NvOptimusEnablement = 1;
+}
+
+//enable laptop AMD gpu
+extern "C"
+{
+	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+
 int main()
 {
 	Application app(Options::windowName, (int)Options::windowWidth, (int)Options::windowHeight);
