ac867fb6a1262d22987448d7b27c08e065745860
Enzo Crisóstomo <enzo.crisostomo2000@gmail.com>
2022-08-05 17:17:39 -0300

    templates em botao

--- ProjetoTransversal/src/OpenGL/VertexBuffer.cpp
+++ ProjetoTransversal/src/OpenGL/VertexBuffer.cpp
@@ -3,46 +3,6 @@
 #include "Vertex.h"
 #include "Chunk/ChunkVertex.h"
 
-template<typename T>
-void VertexBuffer::SetData(const T* data, unsigned int size)
-{
-	glGenBuffers(1, &m_id);
-	glBindBuffer(GL_ARRAY_BUFFER, m_id);
-	glBufferData(GL_ARRAY_BUFFER, size * sizeof(T), static_cast<const void*>(data), GL_STATIC_DRAW);
-}
-
-template<>
-void VertexBuffer::SetData<GLfloat>(const GLfloat* data, unsigned int size)
-{
-	glGenBuffers(1, &m_id);
-	glBindBuffer(GL_ARRAY_BUFFER, m_id);
-	glBufferData(GL_ARRAY_BUFFER, size * sizeof(GLfloat), static_cast<const void*>(data), GL_STATIC_DRAW);
-}
-
-template<>
-void VertexBuffer::SetData<GLuint>(const GLuint* data, unsigned int size)
-{
-	glGenBuffers(1, &m_id);
-	glBindBuffer(GL_ARRAY_BUFFER, m_id);
-	glBufferData(GL_ARRAY_BUFFER, size * sizeof(GLuint), static_cast<const void*>(data), GL_STATIC_DRAW);
-}
-
-template<>
-void VertexBuffer::SetData<Vertex>(const Vertex* data, unsigned int size)
-{
-	glGenBuffers(1, &m_id);
-	glBindBuffer(GL_ARRAY_BUFFER, m_id);
-	glBufferData(GL_ARRAY_BUFFER, size * sizeof(Vertex), static_cast<const void*>(data), GL_STATIC_DRAW);
-}
-
-template<>
-void VertexBuffer::SetData<ChunkVertex>(const ChunkVertex* data, unsigned int size)
-{
-	glGenBuffers(1, &m_id);
-	glBindBuffer(GL_ARRAY_BUFFER, m_id);
-	glBufferData(GL_ARRAY_BUFFER, size * sizeof(ChunkVertex), static_cast<const void*>(data), GL_STATIC_DRAW);
-}
-
 VertexBuffer::~VertexBuffer()
 {
 	DeleteData();
--- ProjetoTransversal/src/OpenGL/VertexBuffer.h
+++ ProjetoTransversal/src/OpenGL/VertexBuffer.h
@@ -10,7 +10,12 @@ public:
 	void Unbind() const;
 
 	template<typename T>
-	void SetData(const T* data, unsigned int size);
+	void SetData(const T* data, unsigned int size)
+	{
+		glGenBuffers(1, &m_id);
+		glBindBuffer(GL_ARRAY_BUFFER, m_id);
+		glBufferData(GL_ARRAY_BUFFER, size * sizeof(T), static_cast<const void*>(data), GL_STATIC_DRAW);
+	}
 
 	void DeleteData();
 
